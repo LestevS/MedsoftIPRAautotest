a21128f5313e5103eb8d54203cc410cec177b3ab
Serg <lestevserg@yandex.ru>
2021-10-31 13:47:38 +0300

    Добавление Констант
    Добавлен файл с Константами в Package Constants
    Заполнять файл согласно советам в комментах.

---
+++ src/main/java/constants/constants.java
@@ -0,0 +1,16 @@
+package constants;
+
+//Если константа прявязана к ID,то имя констаныт=ID
+//Если константа имеет нигативное значение,то имя константы +1-Login1
+//Если ID слишком длинный-можно сократить
+
+public final class constants {
+    public static String login = "mo1";
+    public static String password = "mo1";
+    public static String patLastName = "Иванов";
+    public static String patFirstName = "Иван";
+    public static String patSecName = "Иванович";
+    public static String patBirthDate = "25.07.1990";
+    public static String patSnils = "123-321-123 22";
+
+}
